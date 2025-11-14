b0e66478f5a72cb3a2c8f4218c62c362a80a03c8
GraBoragno <graboragno2005@gmail.com>
2025-11-14 11:10:48 -0300

    inicial

---
+++ src/main/java/webapp/progAvanzada/ProgAvanzadaApplication.java
@@ -0,0 +1,13 @@
+package webapp.progAvanzada;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProgAvanzadaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProgAvanzadaApplication.class, args);
+	}
+
+}
